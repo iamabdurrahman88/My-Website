2f033e000ef291958e2902cd69b093cad17ea950
iamabdurrahman88 <126248622+iamabdurrahman88@users.noreply.github.com>
2023-10-16 01:02:59 +0530

    Add files via upload

---
+++ Projects/Sunlo/js/music-list.js
@@ -0,0 +1,260 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Excuses",
+    artist: "AP Dhillon, Intense",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ma Belle",
+    artist: "AP Dhillon",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Desires",
+    artist: "AP Dhillon",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Insane",
+    artist: "AP Dhillon",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Brown Munde",
+    artist: "AP Dhillon",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Summer High",
+    artist: "AP Dhillon",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Tere Te",
+    artist: "AP Dhillon",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Droptop",
+    artist: "AP Dhillon",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "True Stories",
+    artist: "AP Dhillon",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Dil Nu",
+    artist: "AP Dhillon",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Blue Eyes",
+    artist: "Yo Yo Honey Singh",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Brown Rang",
+    artist: "Yo Yo Honey Singh",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "High Heels",
+    artist: "Yo Yo Honey Singh",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Love Dose",
+    artist: "Yo Yo Honey Singh",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Dope Shope",
+    artist: "Yo Yo Honey Singh",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Paris Ka Trip",
+    artist: "Yo Yo Honey Singh",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Desi Kalakaar",
+    artist: "Yo Yo Honey Singh",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "One Thousand Miles",
+    artist: "NCS Release",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Chaar Botal Vodka",
+    artist: "Yo Yo Honey Singh",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Dheere Dheere",
+    artist: "Yo Yo Honey Singh",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Satisfya",
+    artist: "Imran Khan",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Amplifier",
+    artist: "Imran Khan",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "Bewafa",
+    artist: "Imran Khan",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "Knightridah",
+    artist: "Imran Khan",
+    img: "music-24",
+    src: "music-24"
+  },
+  {
+    name: "Aaja We Mahi ",
+    artist: "Imran Khan",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "Pata Chalgea ",
+    artist: "Imran Khan",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "Imaginary",
+    artist: "Imran Khan",
+    img: "music-27",
+    src: "music-27"
+  },
+  {
+    name: "President Roley",
+    artist: "Imran Khan",
+    img: "music-28",
+    src: "music-28"
+  },
+  {
+    name: "Hey Girl",
+    artist: "Imran Khan",
+    img: "music-29",
+    src: "music-29"
+  },
+  {
+    name: "On My Way",
+    artist: "Imran Khan",
+    img: "music-30",
+    src: "music-30"
+  },
+  {
+    name: "We Rollin",
+    artist: "Shubh",
+    img: "music-31",
+    src: "music-31"
+  },
+  {
+    name: "Offshore",
+    artist: "Shubh",
+    img: "music-32",
+    src: "music-32"
+  },
+  {
+    name: "Baller",
+    artist: "Shubh",
+    img: "music-33",
+    src: "music-33"
+  },
+  {
+    name: "Elevated",
+    artist: "Shubh",
+    img: "music-34",
+    src: "music-34"
+  },
+  {
+    name: "No Love ",
+    artist: "Shubh",
+    img: "music-35",
+    src: "music-35"
+  },
+  {
+    name: "Cheques",
+    artist: "Shubh",
+    img: "music-36",
+    src: "music-36"
+  },
+  {
+    name: "OG",
+    artist: "Shubh",
+    img: "music-37",
+    src: "music-37"
+  },
+  {
+    name: "Her",
+    artist: "Shubh",
+    img: "music-38",
+    src: "music-38"
+  },
+  {
+    name: "Dior",
+    artist: "Shubh",
+    img: "music-39",
+    src: "music-39"
+  },
+  {
+    name: "The Flow",
+    artist: "Shubh",
+    img: "music-40",
+    src: "music-40"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
